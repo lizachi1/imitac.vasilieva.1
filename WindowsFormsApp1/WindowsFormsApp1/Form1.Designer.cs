22e6666a82d512f1fc8451976db1c79f14b190d6
lizachi1 <102459926+lizachi1@users.noreply.github.com>
2023-03-25 01:19:34 +0700

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btСтарт = new System.Windows.Forms.Button();
+            this.edSpeed = new System.Windows.Forms.NumericUpDown();
+            this.edAngle = new System.Windows.Forms.NumericUpDown();
+            this.edHeight = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Высота = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.edSize = new System.Windows.Forms.NumericUpDown();
+            this.edWeight = new System.Windows.Forms.NumericUpDown();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edSpeed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edAngle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edWeight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.edWeight);
+            this.panel1.Controls.Add(this.edSize);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btСтарт);
+            this.panel1.Controls.Add(this.edSpeed);
+            this.panel1.Controls.Add(this.edAngle);
+            this.panel1.Controls.Add(this.edHeight);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.Высота);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(673, 167);
+            this.panel1.TabIndex = 0;
+            // 
+            // btСтарт
+            // 
+            this.btСтарт.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.btСтарт.Font = new System.Drawing.Font("Circe", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btСтарт.Location = new System.Drawing.Point(522, 71);
+            this.btСтарт.Name = "btСтарт";
+            this.btСтарт.Size = new System.Drawing.Size(108, 31);
+            this.btСтарт.TabIndex = 4;
+            this.btСтарт.Text = "Запуск";
+            this.btСтарт.UseVisualStyleBackColor = false;
+            this.btСтарт.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // edSpeed
+            // 
+            this.edSpeed.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.edSpeed.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.edSpeed.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.edSpeed.Location = new System.Drawing.Point(160, 122);
+            this.edSpeed.Name = "edSpeed";
+            this.edSpeed.Size = new System.Drawing.Size(83, 22);
+            this.edSpeed.TabIndex = 1;
+            this.edSpeed.Value = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            // 
+            // edAngle
+            // 
+            this.edAngle.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.edAngle.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.edAngle.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.edAngle.Location = new System.Drawing.Point(160, 91);
+            this.edAngle.Name = "edAngle";
+            this.edAngle.Size = new System.Drawing.Size(83, 22);
+            this.edAngle.TabIndex = 2;
+            this.edAngle.Value = new decimal(new int[] {
+            35,
+            0,
+            0,
+            0});
+            // 
+            // edHeight
+            // 
+            this.edHeight.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.edHeight.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.edHeight.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.edHeight.Location = new System.Drawing.Point(160, 60);
+            this.edHeight.Name = "edHeight";
+            this.edHeight.Size = new System.Drawing.Size(83, 22);
+            this.edHeight.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Circe", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(26, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 35);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Скорость";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Circe", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(26, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 35);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Угол";
+            // 
+            // Высота
+            // 
+            this.Высота.AutoSize = true;
+            this.Высота.BackColor = System.Drawing.Color.Black;
+            this.Высота.Font = new System.Drawing.Font("Circe", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Высота.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Высота.Location = new System.Drawing.Point(26, 52);
+            this.Высота.Name = "Высота";
+            this.Высота.Size = new System.Drawing.Size(92, 35);
+            this.Высота.TabIndex = 0;
+            this.Высота.Text = "Высота";
+            this.Высота.Click += new System.EventHandler(this.Высота_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Black;
+            this.chart1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.DiagonalRight;
+            chartArea2.AxisX.Maximum = 25D;
+            chartArea2.AxisX.Minimum = 0D;
+            chartArea2.AxisY.Maximum = 6D;
+            chartArea2.AxisY.Minimum = 0D;
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Location = new System.Drawing.Point(32, 191);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry;
+            series2.BorderColor = System.Drawing.Color.White;
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series2.Color = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(133)))), ((int)(((byte)(236)))));
+            series2.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(598, 428);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Circe", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(292, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 35);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Размер";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Circe", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(292, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 35);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Масса";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Circe Bold", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(24, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 43);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Траектоия";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Circe Bold", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Location = new System.Drawing.Point(290, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 43);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Тело";
+            // 
+            // edSize
+            // 
+            this.edSize.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.edSize.DecimalPlaces = 2;
+            this.edSize.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.edSize.Location = new System.Drawing.Point(388, 60);
+            this.edSize.Name = "edSize";
+            this.edSize.Size = new System.Drawing.Size(83, 22);
+            this.edSize.TabIndex = 9;
+            this.edSize.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            // 
+            // edWeight
+            // 
+            this.edWeight.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.edWeight.DecimalPlaces = 2;
+            this.edWeight.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.edWeight.Location = new System.Drawing.Point(388, 93);
+            this.edWeight.Name = "edWeight";
+            this.edWeight.Size = new System.Drawing.Size(83, 22);
+            this.edWeight.TabIndex = 10;
+            this.edWeight.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.яку_восстановлено_копnия1;
+            this.ClientSize = new System.Drawing.Size(673, 649);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edSpeed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edAngle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edWeight)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Высота;
+        private System.Windows.Forms.Button btСтарт;
+        private System.Windows.Forms.NumericUpDown edSpeed;
+        private System.Windows.Forms.NumericUpDown edAngle;
+        private System.Windows.Forms.NumericUpDown edHeight;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown edWeight;
+        private System.Windows.Forms.NumericUpDown edSize;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Высота_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        const double dt = 0.1;
+
+        const double g = 9.81;
+        const double C = 0.15;
+        const double rho = 1.29;
+
+
+        double height, angle, speed, S, m;
+        double cosa, sina, beta, k;
+
+        
+
+        double t, x, y, vx, vy;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            height = (double)edHeight.Value;
+            angle = (double)edAngle.Value;
+            speed = (double)edSpeed.Value;
+            S = (double)edSize.Value;
+            m = (double)edWeight.Value;
+
+            cosa = Math.Cos(angle * Math.PI / 180);
+            sina = Math.Sin(angle * Math.PI / 180);
+
+            beta = 0.5 * C * S * rho;
+            k = beta / m;
+
+            t = 0;
+            x = 0;
+            y = height;
+            vx = speed * cosa;
+            vy = speed * sina;
+
+            chart1.Series[0].Points.Clear();
+            chart1.Series[0].Points.AddXY(x, y);
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            double vx_old = vx;
+            double vy_old = vy;
+            double root = Math.Sqrt(vx * vx + vy * vy);
+
+            t = t + dt;
+
+            vx = vx_old - k * vx_old * root * dt;
+            vy = vy_old - (g + k * vy_old * root) * dt;
+
+            x = x + vx * dt;
+            y = y + vy * dt;
+
+            chart1.Series[0].Points.AddXY(x, y);
+
+            if (y <= 0) timer1.Stop();
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
